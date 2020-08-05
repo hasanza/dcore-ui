335006927dad22693b05e810d786412a274bd28d
hasanza <hr90@student.london.ac.uk>
2020-08-05 08:01:54 +0500

    added nav

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <GlobalProvider>
       <div className="App">
+        <div className="Nav">
+          <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1564485715965x221424255250930440%2Ficore_logo_neg.png?w=128&h=24&auto=compress&fit=crop&dpr=1" className="logo"/>
+        </div>
         <ProjectList />
         <Questions />
       </div>
